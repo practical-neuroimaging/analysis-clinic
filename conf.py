88446675dded827b24e4f33f967970b9febc817b
Matthew Brett <matthew.brett@gmail.com>
2015-04-27 10:43:40 -0700

    Notes on Klein paper

--- conf.py
+++ conf.py
@@ -19,6 +19,7 @@ import os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('sphinxext'))
 
 # -- General configuration ------------------------------------------------
 
@@ -30,6 +31,8 @@ import os
 # ones.
 extensions = [
     'sphinx.ext.mathjax',
+    'sphinx.ext.autosummary',
+    'math_dollar',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
